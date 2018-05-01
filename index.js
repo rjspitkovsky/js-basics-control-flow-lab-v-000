ac2d227642e0822c7eb9c37e8f0aaff82985a524
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 19:26:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Write your code in this file!
-function scuberGreetingForFeet (someValue) {
-  
+function scuberGreetingForFeet (feet) {
+  let cost 
+  if (feet <= 400) {
+    cost = "This one is on me!"
+  }
+  else if (feet > 2000 && feet < 2500) {
+    cost = "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500) {
+    cost = "No can do."
+  }
+  return cost 
 }
